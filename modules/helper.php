cf188e5a2958f0bdf9fea58d23fb217fd7e67296
noya <noya@noya-code.(none)>
2015-08-15 13:02:59 +0300

    debug off

--- modules/helper.php
+++ modules/helper.php
@@ -30,12 +30,7 @@ class helper{
 
         $this->template->assign('data', $data);
         $view = $this->template->fetch(strtolower($template).'.tpl');
-        print_r(array($to, $subj, $view, $headers));
-        try{
-        print_r(array(mail($to, $subj, $view, $headers)));die;
-        }catch(Exception $e) {
-            print_r($e->getMessage());die;
-        }
+        mail($to, $subj, $view, $headers);
     }
 
     public function getTemplatePath($template)
